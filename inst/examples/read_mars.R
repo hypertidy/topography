ff0f5ea92a47c36be4de600a9c4fb11fd1a302c2
Michael Sumner <mdsumner@gmail.com>
2021-05-31 06:45:52 +0000

    Read Mars, KSR

---
+++ inst/examples/read_mars.R
@@ -0,0 +1,27 @@
+.tp_standard_version_check <- function() {
+  vers <- try(packageVersion("vapour"), silent = TRUE)
+  if (inherits(vers, "try-error")) vers <- "0."
+  if (vers < "0.5.5.9602") {
+    stop("the development version of {vapour} is required, at least 0.5.5.9602
+         to install please run
+         remotes::install_github(\"hypertidy/vapour\")
+         you might need to install.packages(\"remotes\") first ;)
+         and if an earlier version of {vapour} is installed try restarting R first
+         ")
+  }
+
+}
+
+
+read_mars <- function(xylim = NULL, ...) {
+  .tp_standard_version_check()
+  ext <- topography:::.tp_gebco_defaultgrid(xylim)
+
+  vfile <- topography::topography_source("mars")
+  prj <- ext@crs@projargs
+  if (is.na(prj) || nchar(prj) == 0) stop("no valid projection metadata on 'xylim', this must be present")
+  l <- vapour::vapour_warp_raster(vfile, band = 1, extent = raster::extent(ext), dimension = dim(ext)[2:1],
+                                  wkt = vapour::vapour_srs_wkt(raster::projection(ext)),
+                                  resample = "bilinear")
+  raster::setValues(raster::brick(ext, ext, ext), matrix(unlist(l), ncol = 1))
+}

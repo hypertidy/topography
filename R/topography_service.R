81261d87bd8439d03ab7e7072157f4724dc8404b
Michael Sumner <mdsumner@gmail.com>
2022-07-04 10:43:08 +1000

    cop30, cop90, rejig the sources

--- R/topography_service.R
+++ R/topography_service.R
@@ -32,17 +32,19 @@ topography_source <- function(x = c("gebco", "aws_elevation", "mars", "rema_100m
 #' @examples
 #' topography_services()
 topography_services <- function() {
-  tibble::tibble(name = c("GEBCO", "SRTM15Plus", "NASADEM",
-                          "SRTM_GL1", "SRTM_GL3"),
-                 label = c("gebco", "srtm15", "nasadem",  "srtm_gl1", "srtm_gl3"),
+  tibble::tibble(name = c("GEBCO", "AWS_ELEVATION", "SRTM15Plus", "NASADEM",
+                          "SRTM_GL1", "SRTM_GL3", "COP30", "COP90"),
+                 label = c("gebco", "aws-elevation", "srtm15", "nasadem",  "srtm_gl1", "srtm_gl3", "cop30", "cop90"),
                  file =
-                   c( topography_source_text("gebco"),
+                   c( topography_source_text("gebco"), topography_source_text("aws_elevation"),
 
                       "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM15Plus/SRTM15Plus_srtm.vrt",
                       "/vsicurl/https://opentopography.s3.sdsc.edu/raster/NASADEM/NASADEM_be.vrt",
 
                       "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM_GL1/SRTM_GL1_srtm.vrt",
-                      "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM_GL3/SRTM_GL3_srtm.vrt"
+                      "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM_GL3/SRTM_GL3_srtm.vrt",
+                      "/vsicurl/https://opentopography.s3.sdsc.edu/raster/COP30/COP30_hh.vrt",
+                      "/vsicurl/https://opentopography.s3.sdsc.edu/raster/COP90/COP90_hh.vrt"
                    )
   )
 }
@@ -50,21 +52,18 @@ topography_services <- function() {
 
 
 more_topography_services <- function() {
-  tibble::tibble(name = c("GEBCO_IMAGE", "AWS_ELEVATION", "HRSC_MOLA_Blend", "REMA_100m", "SRTM15Plus", "NASADEM",
-                          "ID18_Struble_op", "ID18_Struble_hh", "ID18_Struble_be", "SRTM_GL1", "SRTM_GL3"),
-                 label = c("gebco_image", "aws_elevation", "mars", "rema_100m", "srtm15", "nasadem", "id18_struble_op", "id18_struble_hh", "id18_struble_be", "srtm_gl1", "srtm_gl3"),
+  tibble::tibble(name = c("GEBCO_IMAGE",  "HRSC_MOLA_Blend", "REMA_100m",
+                          "ID18_Struble_op", "ID18_Struble_hh", "ID18_Struble_be"),
+                 label = c("gebco_image", "mars", "rema_100m",  "id18_struble_op", "id18_struble_hh", "id18_struble_be"),
                  file =
                     c( topography_source_text("gebco_image"),
-                       topography_source_text("aws_elevation"),
+
                                             "/vsicurl/https://planetarymaps.usgs.gov/mosaic/Mars/HRSC_MOLA_Blend/Mars_HRSC_MOLA_BlendDEM_Global_200mp_v2.tif",
                                             "/vsicurl/ftp://ftp.data.pgc.umn.edu/elev/dem/setsm/REMA/mosaic/v1.1/100m/REMA_100m_dem.tif",
-                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM15Plus/SRTM15Plus_srtm.vrt",
-                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/NASADEM/NASADEM_be.vrt",
                                             "/vsicurl/https://opentopography.s3.sdsc.edu/raster/ID18_Struble/ID18_Struble_op.vrt",
                                             "/vsicurl/https://opentopography.s3.sdsc.edu/raster/ID18_Struble/ID18_Struble_hh.vrt",
-                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/ID18_Struble/ID18_Struble_be.vrt",
-                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM_GL1/SRTM_GL1_srtm.vrt",
-                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/SRTM_GL3/SRTM_GL3_srtm.vrt"
+                                            "/vsicurl/https://opentopography.s3.sdsc.edu/raster/ID18_Struble/ID18_Struble_be.vrt"
+
                     )
                     )
 }
